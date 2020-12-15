8c9fcb88372bef4e8340031a81d3b78b6405ab9e
TomekSzaty <tomek.szatylowicz@gmail.com>
2020-12-15 12:47:00 +0100

    Caculator -Test

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/SimleUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing;
+
+public class SimleUser {
+    private String username;
+
+    public SimleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
