01ee1d52f2f8f817d3b61207d1f631ade30d0e80
TomekSzaty <tomek.szatylowicz@gmail.com>
2020-12-17 15:46:59 +0100

    reformat code

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,21 +3,20 @@ package com.kodilla.testing.collection;
 import java.util.*;
 
 
-
 public class OddNumbersExterminator {
 
 
     public static List<Integer> exterminate(List<Integer> numbers) {
 
-        List<Integer>oddNumbers = new ArrayList<>();
+        List<Integer> oddNumbers = new ArrayList<>();
 
-            for (Integer number : numbers) {
-                if (number % 2 == 0) {
-                    oddNumbers.add(number);
-                }
+        for (Integer number : numbers) {
+            if (number % 2 == 0) {
+                oddNumbers.add(number);
             }
-            return oddNumbers;
         }
+        return oddNumbers;
     }
+}
 
 
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.testing.collection;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,37 +29,34 @@ public class CollectionTestSuite {
     public static void afterAll() {
         System.out.println("Test Suite: end");
     }
-    @DisplayName("when use method on empty List, " +
-            "then should return empty list"
-    )
 
+    @DisplayName("when use method on empty List, " +
+            "then should return empty list"    )
     @Test
     void testCaseOddNumbersExterminatorEmptyList() {
         //Given
         List<Integer> emptyList = new ArrayList<>();
 
         //When
-        List<Integer>listNumbers = OddNumbersExterminator.exterminate(emptyList);
-            System.out.println("Testing " + listNumbers);
+        List<Integer> listNumbers = OddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Testing " + listNumbers);
 
         //Then
         Assert.assertEquals(emptyList, listNumbers);
 
     }
+
     @DisplayName("when use method on normal List, " +
-            "then should return  exterminate contents of list"
-    )
-
-
+            "then should return  exterminate contents of list" )
     @Test
     void testCaseOddNumbersExterminatorNormalList() {
         //Given
-        List<Integer>list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
-        List<Integer>oddList = new ArrayList<>(Arrays.asList(2,4,6));
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
+        List<Integer> oddList = new ArrayList<>(Arrays.asList(2, 4, 6));
 
         //When
-        List<Integer>listNumbers = OddNumbersExterminator.exterminate(list);
-            System.out.println("Testing " + listNumbers);
+        List<Integer> listNumbers = OddNumbersExterminator.exterminate(list);
+        System.out.println("Testing " + listNumbers);
 
         //Then
         Assert.assertEquals(oddList, listNumbers);
