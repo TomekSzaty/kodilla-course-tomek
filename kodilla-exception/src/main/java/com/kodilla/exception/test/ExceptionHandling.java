1450843c5f32deb3f1e98339fcebc6a10f4f0f60
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-01-19 16:46:42 +0100

    Zadanie: obsługa wyjątku

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) throws Exception {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            String result = secondChallenge.probablyIWillThrowException(1.9, 1.4);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("UPS.... EXCEPTION !!! " + e.getMessage());
+        } finally {
+            System.out.println("This is  result of your act.");
+        }
+
+    }
+}
