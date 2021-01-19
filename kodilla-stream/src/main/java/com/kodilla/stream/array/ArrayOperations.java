01c14a7b803d1d2e6e1e5cdc673d93c6cdcca5b3
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-01-19 16:24:53 +0100

    Zadanie Srednia funkcyjnie z tablicy

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,20 +1,19 @@
 package com.kodilla.stream.array;
 
-import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 
 public interface ArrayOperations {
 
-    static OptionalDouble getAverage(int[] numbers) {
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length )
+                .map(n ->numbers[n] )
+                .forEach(System.out::println);
 
-        IntStream numbersOfArray = IntStream.range(numbers[0], numbers.length + 1);
-            numbersOfArray.forEach(System.out ::println);
-
-        OptionalDouble averageOfArray = IntStream.range(numbers[0], numbers.length + 1)
-                .average();
-
-        return averageOfArray;
+        return IntStream.range(0, numbers.length)
+                .map(n ->numbers[n] )
+                .average()
+                .orElse(0);
     }
 }
 
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -2,7 +2,6 @@ package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.OptionalDouble;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,12 +10,12 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage() {
         //Given
-        int[] numbers = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int[] numbers = new int[]{20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,};
 
         //When
-         OptionalDouble averageOfArray =  ArrayOperations.getAverage(numbers);
+         double average =  ArrayOperations.getAverage(numbers);
 
         //Then
-        assertEquals(10.5, averageOfArray.getAsDouble());
+        assertEquals(10.5, average);
     }
 }
