6d83d69df4a29048a9a41e91cb315e89429131bc
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-01-20 15:58:08 +0100

    Zadanie: wiedza o wyjątkach

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,9 +1,9 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
 
-        SecondChallenge secondChallenge = new SecondChallenge();
+        /*SecondChallenge secondChallenge = new SecondChallenge();
 
         try {
             String result = secondChallenge.probablyIWillThrowException(1.9, 1.5);
@@ -13,6 +13,23 @@ public class ExceptionHandling {
         } finally {
             System.out.println("This is  result of your act.");
         }
+*/
+        FlightSearch flightSearch = new FlightSearch();
 
+        try {
+            flightSearch.findFlight(new Flight("New York", "Sydney"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("UPS..." + e.getMessage());
+        } finally {
+            System.out.println("Have a nice day");
+        }
+        System.out.println("****************************************************************");
+        try {
+            flightSearch.findFlight(new Flight("New York", "Los Angeles"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("UPS...Flight not found" );
+        } finally {
+            System.out.println("Have a nice day");
+        }
     }
 }
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSearch {
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> airports = new HashMap<>();
+        airports.put("New York", true);
+        airports.put("Los Angeles", false);
+        airports.put("Sydney", true);
+
+        if (airports.containsKey(flight.getArrivalAirport()) &&
+                (airports.containsKey(flight.getDepartureAirport())) &&
+                (airports.get(flight.getArrivalAirport())) &&
+                (airports.get(flight.getDepartureAirport()))) {
+
+            System.out.println("Have a nice flight");
+
+        } else throw new RouteNotFoundException();
+    }
+}
