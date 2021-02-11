32ff49a43f97c3c66462065e4b383808ddf0c973
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-02-11 10:12:11 +0100

    Zadanie: funkcyjna iteracja po tytułach

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/chalenges/MainStream.java
@@ -0,0 +1,15 @@
+package com.kodilla.good.patterns.chalenges;
+
+import java.util.stream.Collectors;
+
+public class MainStream {
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+        System.out.println(movieStore.getMovies());
+        String challengeMovieStore = movieStore.getMovies().entrySet().stream()
+                .flatMap(e -> e.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(challengeMovieStore);
+    }
+}
