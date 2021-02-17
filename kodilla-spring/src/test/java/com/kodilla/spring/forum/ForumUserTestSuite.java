871303219c1a9a4a7ce5a4c8685f48566307ae5a
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-02-17 08:29:35 +0100

    Zadanie: bean ForumUser

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,32 @@
+package com.kodilla.spring.forum;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class ForumUserTestSuite {
+
+    @Test
+    void testGetUserName() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+         ForumUser forumUser = context.getBean(ForumUser.class);
+
+         //When
+        String userName = forumUser.getUserName();
+
+        //Then
+        assertEquals("John Smith", userName);
+
+    }
+    @Test
+    void contextLoads() {
+    }
+
+}
