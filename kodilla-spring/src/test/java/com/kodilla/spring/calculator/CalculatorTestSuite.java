26617295c35f442e100a09824e0285252f0dabb7
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-02-17 13:30:05 +0100

    Zadanie: kalkulator z wyświetlaczem

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+
+
+    @Test
+    void testCalculationAdd() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double sum = calculator.add(4, 6);
+        //then
+        assertEquals(10, sum);
+    }
+    @Test
+    void testCalculationSub() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double sum = calculator.sub(6, 4);
+        //then
+        assertEquals(2, sum);
+    }
+    @Test
+    void testCalculationMul() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double sum = calculator.mul(6, 4);
+        //then
+        assertEquals(24, sum);
+    }
+    @Test
+    void testCalculationDiv() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double sum = calculator.div(6, 4);
+        //then
+        assertEquals(1.5, sum);
+    }
+}
