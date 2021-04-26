e4b0bd9724bedfa2f650f91fa51cfbef9a541a71
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-04-26 17:18:15 +0200

    Patterns Design - Observer-'ForumTopicObserver'

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/observer/forum/ForumUser.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns2.facade.observer.forum;
+
+public class ForumUser implements Observer {
+
+    private final String userName;
+    private int updateCount;
+
+    public ForumUser(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getUpdateCount() {
+        return updateCount;
+    }
+
+    @Override
+    public void update(ForumTopic forumTopic) {
+        System.out.println(userName + ": New messages in topic  " + forumTopic.getName() + "\n" +
+                " (total: " +forumTopic.getMessages().size() + " messages");
+        updateCount++;
+
+    }
+}
