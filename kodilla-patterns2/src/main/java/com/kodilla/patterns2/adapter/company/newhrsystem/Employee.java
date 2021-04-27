bffab0b762ccc60c1d6a2f0aefc0722e21f1ec95
TomekSzaty <tomek.szatylowicz@gmail.com>
2021-04-27 11:21:18 +0200

    Wzorzec Adapter - "OldHrSystem" and "NewHrSystem"

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/newhrsystem/Employee.java
@@ -0,0 +1,69 @@
+package com.kodilla.patterns2.adapter.company.newhrsystem;
+
+import java.math.BigDecimal;
+
+public class Employee {
+
+    private final String peselId;
+    private final String firstName;
+    private final String lastName;
+    private final BigDecimal  baseSalary;
+
+    public Employee(String peselId, String firstName, String lastName, BigDecimal baseSalary) {
+        this.peselId = peselId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getPeselId() {
+        return peselId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public BigDecimal getBaseSalary() {
+        return baseSalary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+
+        Employee employee = (Employee) o;
+
+        if (getPeselId() != null ? !getPeselId().equals(employee.getPeselId()) : employee.getPeselId() != null)
+            return false;
+        if (getFirstName() != null ? !getFirstName().equals(employee.getFirstName()) : employee.getFirstName() != null)
+            return false;
+        if (getLastName() != null ? !getLastName().equals(employee.getLastName()) : employee.getLastName() != null)
+            return false;
+        return getBaseSalary() != null ? getBaseSalary().equals(employee.getBaseSalary()) : employee.getBaseSalary() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getPeselId() != null ? getPeselId().hashCode() : 0;
+        result = 31 * result + (getFirstName() != null ? getFirstName().hashCode() : 0);
+        result = 31 * result + (getLastName() != null ? getLastName().hashCode() : 0);
+        result = 31 * result + (getBaseSalary() != null ? getBaseSalary().hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "peselId='" + peselId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", baseSalary=" + baseSalary +
+                '}';
+    }
+}
